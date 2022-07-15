4ea2a2cce9245fefc77f3dc57c05f063a60c9e29
Noah Celuch <noah.celuch@gmail.com>
2022-07-14 22:17:45 -0400

    test(user): added tests for join by code screen

---
+++ __tests__/screens/JoinByCodeScreen-test.tsx
@@ -0,0 +1,90 @@
+import * as UserData from '../../src/services/data/user'
+import JoinByCodeScreen from '../../src/screens/JoinByCodeScreen'
+import { NavigationContainer } from '@react-navigation/native'
+import { Props } from '../../src/types/navigation'
+import { Provider } from 'react-redux'
+import React from 'react'
+import { fetchProfileData } from '../../fixtures/data'
+import renderer from 'react-test-renderer'
+import store from '../../src/store/store'
+import { act, fireEvent, render } from '@testing-library/react-native'
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
+
+const navigate = jest.fn()
+
+const props: Props = {
+    navigation: {
+        navigate,
+    } as any,
+    route: {} as any,
+}
+
+beforeEach(() => {
+    navigate.mockReset()
+})
+
+it('should match snapshot', () => {
+    const snapshot = renderer
+        .create(
+            <Provider store={store}>
+                <JoinByCodeScreen {...props} />
+            </Provider>,
+        )
+        .toJSON()
+
+    expect(snapshot).toMatchSnapshot()
+})
+
+it('should handle failed code submission', async () => {
+    const spy = jest
+        .spyOn(UserData, 'joinTeamByCode')
+        .mockReturnValueOnce(Promise.reject({ message: 'Cannot join team' }))
+
+    const { getByText, getByPlaceholderText } = render(
+        <Provider store={store}>
+            <NavigationContainer>
+                <JoinByCodeScreen {...props} />
+            </NavigationContainer>
+        </Provider>,
+    )
+
+    const input = getByPlaceholderText('6 Digit Code')
+    const button = getByText('join')
+
+    fireEvent.changeText(input, '123456')
+    fireEvent.press(button)
+    await act(async () => {})
+
+    expect(spy).toHaveBeenCalled()
+    expect(getByText('Cannot join team')).toBeTruthy()
+})
+
+it('should handle successful code submission', async () => {
+    const spy = jest
+        .spyOn(UserData, 'joinTeamByCode')
+        .mockReturnValueOnce(Promise.resolve(fetchProfileData))
+
+    const { getByText, getByPlaceholderText } = render(
+        <Provider store={store}>
+            <NavigationContainer>
+                <JoinByCodeScreen {...props} />
+            </NavigationContainer>
+        </Provider>,
+    )
+
+    const input = getByPlaceholderText('6 Digit Code')
+    const button = getByText('join')
+
+    fireEvent.changeText(input, '123456')
+    fireEvent.press(button)
+    await act(async () => {})
+
+    expect(spy).toHaveBeenCalled()
+    expect(getByText('Success!')).toBeTruthy()
+
+    const doneButton = getByText('Done')
+    fireEvent.press(doneButton)
+
+    expect(navigate).toHaveBeenCalledWith('ManageTeams')
+})
