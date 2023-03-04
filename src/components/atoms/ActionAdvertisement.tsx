b73b75de71f021587e8c631a28d1495a77ff8098
Noah Celuch <noah.celuch@gmail.com>
2023-03-04 16:43:01 -0500

    test(ad): updated tests for new ad functionality

--- src/components/atoms/ActionAdvertisement.tsx
+++ src/components/atoms/ActionAdvertisement.tsx
@@ -1,8 +1,18 @@
+import { Platform } from 'react-native'
 import React from 'react'
+import { ANDROID_AD_UNIT_ID, IOS_AD_UNIT_ID } from '@env'
 import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads'
 
 const ActionAdvertisement: React.FC<{}> = () => {
-    return <BannerAd unitId={TestIds.BANNER} size={BannerAdSize.BANNER} />
+    const unitId = React.useMemo(() => {
+        if (process.env.NODE_ENV === 'production') {
+            return Platform.OS === 'ios' ? IOS_AD_UNIT_ID : ANDROID_AD_UNIT_ID
+        } else {
+            return TestIds.BANNER
+        }
+    }, [])
+
+    return <BannerAd unitId={unitId} size={BannerAdSize.BANNER} />
 }
 
 export default ActionAdvertisement
