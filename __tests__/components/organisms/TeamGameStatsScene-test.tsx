f6e3cee40b39d3a241271ca54ef98584e6942922
Noah Celuch <noah.celuch@gmail.com>
2023-06-02 22:29:56 -0400

    test(stats): tests for team stats scene

--- __tests__/components/organisms/TeamGameStatsScene-test.tsx
+++ __tests__/components/organisms/TeamGameStatsScene-test.tsx
@@ -1,9 +1,16 @@
 import * as StatsData from '../../../src/services/data/stats'
-import { GameStats } from '../../../src/types/stats'
 import React from 'react'
 import TeamGameStatsScene from '../../../src/components/organisms/TeamGameStatsScene'
+import { teamOne } from '../../../fixtures/data'
+import {
+    FilteredGamePlayer,
+    GameStats,
+    TeamStats,
+} from '../../../src/types/stats'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { render, screen, waitFor } from '@testing-library/react-native'
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 
 const playerOne = {
     _id: 'user1',
@@ -42,6 +49,54 @@ const gameStats: GameStats = {
     },
 }
 
+const teamStats: TeamStats = {
+    ...teamOne,
+    players: [],
+    games: [],
+    winPercentage: 1,
+    offensiveConversion: 0.8,
+    defensiveConversion: 0.4,
+    wins: 10,
+    losses: 0,
+    goalsFor: 100,
+    goalsAgainst: 67,
+    holds: 87,
+    breaks: 13,
+    turnoverFreeHolds: 45,
+    offensePoints: 23,
+    defensePoints: 54,
+    turnovers: 4,
+    turnoversForced: 45,
+}
+
+const player: FilteredGamePlayer = {
+    ...playerOne,
+    goals: 1,
+    assists: 1,
+    blocks: 1,
+    throwaways: 1,
+    throwingPercentage: 0.98,
+    droppedPasses: 0,
+    drops: 1,
+    callahans: 0,
+    stalls: 0,
+    catches: 4,
+    touches: 5,
+    completedPasses: 4,
+    pointsPlayed: 3,
+    pulls: 0,
+    wins: 0,
+    losses: 1,
+    ppAssists: 0.1,
+    ppBlocks: 0.1,
+    ppDrops: 0,
+    ppGoals: 0.1,
+    ppThrowaways: 1,
+    plusMinus: 2,
+    winPercentage: 0.8,
+    catchingPercentage: 1,
+}
+
 const client = new QueryClient()
 
 describe('TeamGameStatsScene', () => {
@@ -57,8 +112,11 @@ describe('TeamGameStatsScene', () => {
     })
 
     it('renders without team id', async () => {
-        const spy = jest
+        const gameSpy = jest
             .spyOn(StatsData, 'getGameStatsByTeam')
+            .mockReturnValue(Promise.reject({}))
+        const teamSpy = jest
+            .spyOn(StatsData, 'getTeamStatsByGame')
             .mockReturnValue(Promise.reject({}))
 
         render(
@@ -68,8 +126,9 @@ describe('TeamGameStatsScene', () => {
         )
 
         await waitFor(() => {
-            expect(spy).toHaveBeenCalledTimes(1)
+            expect(gameSpy).toHaveBeenCalledTimes(1)
         })
+        expect(teamSpy).toHaveBeenCalledTimes(1)
         expect(
             screen.getByText(
                 'This team is not contributing stats for this game.',
@@ -77,10 +136,15 @@ describe('TeamGameStatsScene', () => {
         ).toBeTruthy()
     })
 
-    it('renders leaders', async () => {
-        const spy = jest
+    it('renders populated scene', async () => {
+        const gameSpy = jest
             .spyOn(StatsData, 'getGameStatsByTeam')
-            .mockReturnValue(Promise.resolve({ ...gameStats, players: [] }))
+            .mockReturnValue(
+                Promise.resolve({ ...gameStats, players: [player] }),
+            )
+        jest.spyOn(StatsData, 'getTeamStatsByGame').mockReturnValue(
+            Promise.resolve(teamStats),
+        )
 
         render(
             <QueryClientProvider client={client}>
@@ -91,8 +155,15 @@ describe('TeamGameStatsScene', () => {
         await waitFor(() => {
             expect(screen.getByText('Leaderboard')).toBeTruthy()
         })
-        expect(spy).toHaveBeenCalledTimes(1)
-        expect(screen.getAllByText('1').length).toBe(6)
-        expect(screen.getAllByText('First 1 Last 1').length).toBe(6)
+        expect(gameSpy).toHaveBeenCalledTimes(1)
+        expect(screen.getAllByText('1').length).toBe(11)
+        expect(screen.getAllByText('First 1 Last 1').length).toBe(7)
+        expect(screen.getByText('Stats')).toBeTruthy()
+        expect(screen.getByText('Game Overview')).toBeTruthy()
+        expect(screen.getByText('Leaderboard')).toBeTruthy()
+        expect(screen.getAllByText('Assists').length).toBe(2)
+        expect(screen.getAllByText('Goals').length).toBe(2)
+        expect(screen.getByText('Blocks')).toBeTruthy()
+        expect(screen.getByText('Drops')).toBeTruthy()
     })
 })
