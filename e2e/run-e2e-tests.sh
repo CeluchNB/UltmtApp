b55499f605f12110f6ef0854e47103b6dd08f660
Noah Celuch <noah.celuch@gmail.com>
2023-08-23 22:20:50 -0400

    test(e2e): add tests and stabilize e2e test setup

--- e2e/run-e2e-tests.sh
+++ e2e/run-e2e-tests.sh
@@ -8,6 +8,13 @@ curl -s --max-time 30 --retry 3 --retry-max-time 120 --header "X-API-Key: $API_K
 yarn start:e2e & disown > /dev/null
 PID=$!
 
+# Build Android
+cd ../android ; ./gradlew clean ; cd ../e2e
+detox build -c android.emu.debug
+
+# Build iOS
+detox build -c ios.sim.debug
+
 # Delete data in DBs
 mongosh "mongodb+srv://$MONGO_USERNAME:$MONGO_PASSWORD@ultmt-api.lrtagl9.mongodb.net/ultmt-api" e2e/seed.js > /dev/null
 
@@ -15,10 +22,10 @@ mongosh "mongodb+srv://$MONGO_USERNAME:$MONGO_PASSWORD@ultmt-api.lrtagl9.mongodb
 detox test -c android.emu.debug e2e/team-management.test.ts
 
 # Delete data in DBs
-# mongosh "mongodb+srv://$MONGO_USERNAME:$MONGO_PASSWORD@ultmt-api.lrtagl9.mongodb.net/ultmt-api" e2e/seed.js > /dev/null
+mongosh "mongodb+srv://$MONGO_USERNAME:$MONGO_PASSWORD@ultmt-api.lrtagl9.mongodb.net/ultmt-api" e2e/seed.js > /dev/null
 
 # Test ios
-# detox test -c ios.sim.debug
+detox test -c ios.sim.debug e2e/team-management.test.ts
 
 # Kill metro
 kill $PID
--- e2e/team-management.test.ts
+++ e2e/team-management.test.ts
@@ -1,49 +1,26 @@
+import { by, device, element, expect } from 'detox'
+
 describe('Team Management', () => {
     beforeAll(async () => {
+        await device.uninstallApp()
+        await device.installApp()
         await device.launchApp({ newInstance: true })
         await element(by.id('account')).tap()
         await createAccount('Ignacio', 'Varga', 'nacho')
         await createAccount('Gustavo', 'Fring', 'gus')
         await createAccount('Jesse', 'Pinkman', 'jesse')
         await createAccount('Walter', 'White', 'heisenberg')
-
-        // // skyler
-        // await element(by.text('CREATE ACCOUNT')).tap()
-        // await element(by.label('First Name')).atIndex(0).typeText('Skyler')
-        // await element(by.label('Last Name')).atIndex(0).typeText('White')
-        // await element(by.label('Username')).atIndex(0).typeText('sky')
-        // await element(by.label('Email')).atIndex(0).typeText('sky@email.com')
-        // await element(by.label('Password')).atIndex(0).typeText('12Pass!!')
-        // await element(by.text('CREATE')).tap()
-        // await expect(element(by.text('Skyler White'))).toBeVisible()
-        // await element(by.text('SIGN OUT')).tap()
-
-        // // lalo
-        // await element(by.text('CREATE ACCOUNT')).tap()
-        // await element(by.label('First Name')).atIndex(0).typeText('Eduardo')
-        // await element(by.label('Last Name')).atIndex(0).typeText('Salamanca')
-        // await element(by.label('Username')).atIndex(0).typeText('lalo')
-        // await element(by.label('Email')).atIndex(0).typeText('lalo@email.com')
-        // await element(by.label('Password')).atIndex(0).typeText('12Pass!!')
-        // await element(by.text('CREATE')).tap()
-        // await expect(element(by.text('Eduardo Salamanca'))).toBeVisible()
-        // await element(by.text('SIGN OUT')).tap()
-
-        // // hector
-        // await element(by.text('CREATE ACCOUNT')).tap()
-        // await element(by.label('First Name')).atIndex(0).typeText('Hector')
-        // await element(by.label('Last Name')).atIndex(0).typeText('Salamanca')
-        // await element(by.label('Username')).atIndex(0).typeText('hector')
-        // await element(by.label('Email')).atIndex(0).typeText('hector@email.com')
-        // await element(by.label('Password')).atIndex(0).typeText('12Pass!!')
-        // await element(by.text('CREATE')).tap()
-        // await expect(element(by.text('Hector Salamanca'))).toBeVisible()
-        // await element(by.text('SIGN OUT')).tap()
+        await createAccount('Skyler', 'White', 'sky')
     })
 
     beforeEach(async () => {
+        await device.uninstallApp()
+        await device.installApp()
         await device.launchApp({ newInstance: true })
         await element(by.id('account')).tap()
+        try {
+            await element(by.text('SIGN OUT')).tap()
+        } catch {}
     })
 
     it('Create team', async () => {
@@ -53,20 +30,22 @@ describe('Team Management', () => {
         await element(by.id('team-place-input')).typeText('Albuquerque')
         await element(by.id('team-name-input')).typeText('Cookers')
         await element(by.id('team-handle-input')).typeText('acook')
+        await element(by.id('team-handle-input')).tapReturnKey()
         await element(by.text('CREATE')).tap()
         await expect(element(by.text('Managing'))).toBeVisible()
         await expect(element(by.text('Albuquerque Cookers'))).toBeVisible()
         await expect(element(by.text('@acook'))).toBeVisible()
     })
 
-    it('Player perform request and team accept request', async () => {
-        await element(by.text('SIGN OUT')).tap()
+    it('Accept player request to join team', async () => {
         await login('gus')
         await element(by.text('MANAGE TEAMS')).tap()
         await element(by.id('create-button')).atIndex(0).tap()
         await element(by.id('search-input')).atIndex(0).typeText('acook')
         await element(by.id('search-result-item')).tap()
-        await element(by.id('search-result-item')).tap()
+        if (device.getPlatform() === 'android') {
+            await element(by.id('search-result-item')).tap()
+        }
         await element(by.id('account')).tap()
         await element(by.text('SIGN OUT')).tap()
         await login('nacho')
@@ -74,13 +53,15 @@ describe('Team Management', () => {
         await element(by.text('@acook')).tap()
         await element(by.text('MANAGE REQUESTS')).tap()
         await element(by.id('accept-button')).tap()
-        // FIX FOR iOS
-        await device.pressBack()
+        await element(by.id('account')).tap()
+        await element(by.text('MANAGE TEAMS')).tap()
+        await element(by.text('@acook')).tap()
         await expect(element(by.text('Gustavo Fring'))).toBeVisible()
         await expect(element(by.text('@gus'))).toBeVisible()
     })
 
     it('Accept team request to join team', async () => {
+        await login('nacho')
         await element(by.id('account')).tap()
         await element(by.text('MANAGE TEAMS')).tap()
         await element(by.text('@acook')).tap()
@@ -93,20 +74,21 @@ describe('Team Management', () => {
         await login('jesse')
         await element(by.text('Requests')).tap()
         await element(by.id('accept-button')).tap()
-        await device.pressBack()
+        await element(by.id('account')).tap()
         await element(by.id('profile-flat-list')).swipe('down')
         await expect(element(by.text('Albuquerque Cookers'))).toBeVisible()
         await expect(element(by.text('@acook'))).toBeVisible()
     })
 
     it('Deny player request to join team', async () => {
-        await element(by.text('SIGN OUT')).tap()
         await login('heisenberg')
         await element(by.text('Requests')).tap()
         await element(by.id('create-button')).tap()
         await element(by.id('search-input')).atIndex(0).typeText('acook')
         await element(by.id('search-result-item')).tap()
-        await element(by.id('search-result-item')).tap()
+        if (device.getPlatform() === 'android') {
+            await element(by.id('search-result-item')).tap()
+        }
         await element(by.id('account')).tap()
         await element(by.text('SIGN OUT')).tap()
         await login('nacho')
@@ -114,7 +96,9 @@ describe('Team Management', () => {
         await element(by.text('@acook')).tap()
         await element(by.text('MANAGE REQUESTS')).tap()
         await element(by.id('delete-button')).atIndex(0).tap()
-        await device.pressBack()
+        await element(by.id('account')).tap()
+        await element(by.text('MANAGE TEAMS')).tap()
+        await element(by.text('@acook')).tap()
         await expect(element(by.text('@heisenberg'))).not.toBeVisible()
         await element(by.id('account')).tap()
         await element(by.text('SIGN OUT')).tap()
@@ -124,11 +108,30 @@ describe('Team Management', () => {
         await expect(element(by.text('Denied'))).toBeVisible()
     })
 
-    // it('Deny team request to join team', async () => {})
+    it('Deny team request to join team', async () => {
+        await login('nacho')
+        await element(by.text('MANAGE TEAMS')).tap()
+        await element(by.text('@acook')).tap()
+        await element(by.id('create-button')).atIndex(1).tap()
+        await element(by.id('search-input')).atIndex(0).typeText('sky')
+        await element(by.text('@sky')).tap()
+        await element(by.text('@sky')).tap()
+        await element(by.id('account')).tap()
+        await element(by.text('SIGN OUT')).tap()
+        await login('sky')
+        await element(by.text('Requests')).tap()
+        await element(by.id('delete-button')).tap()
+        await element(by.id('account')).tap()
+        await element(by.id('profile-flat-list')).swipe('down')
+        await expect(element(by.text('Albuquerque Cookers'))).not.toBeVisible()
+        await expect(element(by.text('@acook'))).not.toBeVisible()
+    })
 
     const login = async (username: string) => {
-        await element(by.label('Username or Email')).typeText(username)
-        await element(by.label('Password')).typeText('12Pass!!')
+        await element(by.label('Username or Email'))
+            .atIndex(0)
+            .typeText(username)
+        await element(by.label('Password')).atIndex(0).typeText('12Pass!!')
         await element(by.text('LOGIN')).tap()
     }
 
@@ -137,6 +140,10 @@ describe('Team Management', () => {
         lastName: string,
         username: string,
     ) => {
+        await element(by.id('account')).tap()
+        try {
+            await element(by.text('SIGN OUT')).tap()
+        } catch {}
         await element(by.text('CREATE ACCOUNT')).tap()
         await element(by.label('First Name')).atIndex(0).typeText(firstName)
         await element(by.label('Last Name')).atIndex(0).typeText(lastName)
