197ff579eaec850a05dcf27d9175824c62d0f575
Noah Celuch <noah.celuch@gmail.com>
2024-07-15 22:38:38 -0400

    fix(action): undo action fix

--- src/hooks/game-edit-actions/use-undo-action.ts
+++ src/hooks/game-edit-actions/use-undo-action.ts
@@ -1,6 +1,5 @@
 import { ActionSchema } from '../../models'
 import { LiveGameContext } from '../../context/live-game-context'
-import { TeamNumber } from '../../types/team'
 import { handleUndoActionSideEffects } from '../../services/data/live-action'
 import { parseAction } from '../../utils/action'
 import { useContext } from 'react'
@@ -11,39 +10,27 @@ export const useUndoAction = () => {
     const { point } = useContext(LiveGameContext)
     const realm = useRealm()
 
-    return useMutation(async () =>
-        //     {
-        //     actionNumber,
-        //     team,
-        // }: {
-        //     actionNumber: number
-        //     team: TeamNumber
-        // }
-        {
-            if (!point) return
+    return useMutation(async () => {
+        if (!point) return
 
-            const actions = realm
-                .objects<ActionSchema>('Action')
-                .filtered('pointId == $0', point._id)
-                .sorted('actionNumber', true)
-            if (actions.length < 1) return
+        const actions = realm
+            .objects<ActionSchema>('Action')
+            .filtered('pointId == $0', point._id)
+            .sorted('actionNumber', true)
+        if (actions.length < 1) return
 
-            // This convoluted method appears to fix the undo error where
-            // actions were sometimes not deleted. Continue to monitor
-            const { _id } = actions[0]
-            const action = realm.objectForPrimaryKey<ActionSchema>(
-                'Action',
-                _id,
-            )
-            if (!action) return
+        // This convoluted method appears to fix the undo error where
+        // actions were sometimes not deleted. Continue to monitor
+        const { _id } = actions[0]
+        const action = realm.objectForPrimaryKey<ActionSchema>('Action', _id)
+        if (!action) return
 
-            const { actionNumber } = action
-            realm.write(() => {
-                handleUndoActionSideEffects(parseAction(actions[0]), point)
-                realm.delete(action)
-            })
+        const { actionNumber } = action
+        realm.write(() => {
+            handleUndoActionSideEffects(parseAction(actions[0]), point)
+            realm.delete(action)
+        })
 
-            return actionNumber
-        },
-    )
+        return actionNumber
+    })
 }
