8100b2de35eaf9b5644f65b448ff60d3b86eec5c
Noah Celuch <noah.celuch@gmail.com>
2024-07-24 17:27:02 -0400

    test: fix search tournament screen tests

--- __tests__/screens/games/SearchTournamentScreen-test.tsx
+++ __tests__/screens/games/SearchTournamentScreen-test.tsx
@@ -6,7 +6,16 @@ import { SearchTournamentProps } from '../../../src/types/navigation'
 import SearchTournamentScreen from '../../../src/screens/games/SearchTournamentScreen'
 import store from '../../../src/store/store'
 import { tourney } from '../../../fixtures/data'
-import { act, fireEvent, render, waitFor } from '@testing-library/react-native'
+import {
+    act,
+    fireEvent,
+    render,
+    screen,
+    waitFor,
+} from '@testing-library/react-native'
+import { withRealm } from '../../utils/renderers'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { CreateGameProvider } from '../../../src/context/create-game-context'
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper')
 
 const goBack = jest.fn()
@@ -15,13 +24,23 @@ const props: SearchTournamentProps = {
     route: {} as any,
 }
 
+const client = new QueryClient()
+
 describe('SearchTournamentScreen', () => {
+    beforeAll(() => {
+        jest.useFakeTimers({ legacyFakeTimers: true })
+    })
+
+    afterAll(() => {
+        jest.useRealTimers()
+    })
+
     afterEach(() => {
         jest.clearAllMocks()
     })
 
     it('renders', async () => {
-        const { getByPlaceholderText } = render(
+        render(
             <Provider store={store}>
                 <NavigationContainer>
                     <SearchTournamentScreen {...props} />
@@ -29,7 +48,9 @@ describe('SearchTournamentScreen', () => {
             </Provider>,
         )
         await act(async () => {})
-        expect(getByPlaceholderText('Search Tournaments...')).toBeTruthy()
+        expect(
+            screen.getByPlaceholderText('Search Tournaments...'),
+        ).toBeTruthy()
     })
 
     it('handles search and click', async () => {
@@ -37,11 +58,17 @@ describe('SearchTournamentScreen', () => {
             .spyOn(TournamentData, 'searchTournaments')
             .mockReturnValue(Promise.resolve([tourney]))
         const { getByText, getByPlaceholderText } = render(
-            <Provider store={store}>
-                <NavigationContainer>
-                    <SearchTournamentScreen {...props} />
-                </NavigationContainer>
-            </Provider>,
+            withRealm(
+                <Provider store={store}>
+                    <NavigationContainer>
+                        <QueryClientProvider client={client}>
+                            <CreateGameProvider>
+                                <SearchTournamentScreen {...props} />
+                            </CreateGameProvider>
+                        </QueryClientProvider>
+                    </NavigationContainer>
+                </Provider>,
+            ),
         )
 
         const input = getByPlaceholderText('Search Tournaments...')
@@ -55,7 +82,6 @@ describe('SearchTournamentScreen', () => {
         const tourneyItem = getByText(tourney.name)
         fireEvent.press(tourneyItem)
 
-        expect(store.getState().liveGame.game.tournament).toMatchObject(tourney)
         expect(goBack).toHaveBeenCalled()
     })
 
