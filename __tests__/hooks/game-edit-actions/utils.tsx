221c5d14a93cb318fbd3df5c5b99cac38510f4f8
Noah Celuch <noah.celuch@gmail.com>
2024-08-26 23:11:13 -0400

    test: create tournament and delete game tests

--- __tests__/hooks/game-edit-actions/utils.tsx
+++ __tests__/hooks/game-edit-actions/utils.tsx
@@ -1,10 +1,6 @@
 import { RealmProvider } from '@ultmt-app/context/realm'
-import {
-    LiveGameContext,
-    LiveGameContextData,
-} from '@ultmt-app/context/live-game-context'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import React, { ReactNode, useEffect } from 'react'
+import React, { ReactNode } from 'react'
 
 const client = new QueryClient()
 export const TopLevelComponent = ({ children }: { children: ReactNode }) => {
@@ -14,28 +10,5 @@ export const TopLevelComponent = ({ children }: { children: ReactNode }) => {
                 {children}
             </QueryClientProvider>
         </RealmProvider>
-    )
-}
-
-export const LiveGameComponent = ({
-    children,
-    init,
-    args,
-}: {
-    children: ReactNode
-    init: () => {}
-    args: Partial<LiveGameContextData>
-}) => {
-    useEffect(() => {
-        init()
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [])
-
-    return (
-        <TopLevelComponent>
-            <LiveGameContext.Provider value={args as LiveGameContextData}>
-                {children}
-            </LiveGameContext.Provider>
-        </TopLevelComponent>
     )
 }
